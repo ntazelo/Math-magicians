88cc1318a8cd0b94111b4fd54b7b71c86a5d8a9c
ntazelo <ntazelo@gmail.com>
2021-11-24 18:21:35 +0200

    Import calculator component into app component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import Calculator from './components/Calculator';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
